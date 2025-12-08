b74f339a31839edb5801877d9786a5e305d6991a
Nagendra <nagendra.anasuri08@gmail.com>
2025-12-08 20:08:28 +0530

    Initial commit - account microfrontend

---
+++ src/AccountApp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AccountApp() {
+  return (
+    <div style={{ border: "2px solid green", padding: 16 }}>
+      <h2>Account Microfrontend</h2>
+      <p>User profile / settings will go here.</p>
+    </div>
+  );
+}
